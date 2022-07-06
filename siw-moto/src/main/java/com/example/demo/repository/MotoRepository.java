2eddd72a4d6e7997cba0bfc80d4a1aa0a6b9ad0b
giucaciari <giucacio1609@gmail.com>
2022-07-06 16:26:32 +0200

    modifiche su htlm

--- siw-moto/src/main/java/com/example/demo/repository/MotoRepository.java
+++ siw-moto/src/main/java/com/example/demo/repository/MotoRepository.java
@@ -6,5 +6,5 @@ import com.example.demo.model.Moto;
 
 public interface MotoRepository extends CrudRepository<Moto, Long> {
 
-	//public Moto findByNomeAndAnno(String nome, Long anno);
+	//public Moto findByNomeAndAnno(String nome, Long anno);//
 }
