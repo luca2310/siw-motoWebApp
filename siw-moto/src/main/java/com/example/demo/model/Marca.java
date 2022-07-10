e5c360aa6302b1c0e5e0a6acdc6cc9ef6995d9a8
luca2310 <luca.catar@gmail.com>
2022-07-10 11:10:43 +0200

    errore form

--- siw-moto/src/main/java/com/example/demo/model/Marca.java
+++ siw-moto/src/main/java/com/example/demo/model/Marca.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Marca {
@@ -16,6 +17,7 @@ public class Marca {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@NotBlank
 	private String nome;
 	
 	private String origine;
